2328e70838d129f805084826f3c077a0affc9282
NielsBattle <35956413+NielsBattle@users.noreply.github.com>
2019-03-26 18:32:21 +0100

    Deleted readme

--- TraineeBee/config/database.php
+++ TraineeBee/config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'mysql0.mydevil.net'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'm11668_TB'),
-            'username' => env('DB_USERNAME', 'm11668_TB'),
+            'database' => env('DB_DATABASE', 'TraineeBee'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
