f131418b7e1aa87dc7febdbff29296b6e7ff72f5
NielsBattle <35956413+NielsBattle@users.noreply.github.com>
2019-03-26 21:02:37 +0100

    might break shit

---
+++ TraineeBee/resources/views/Pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('Layouts.layout')
+@section('content')
+
+    <h1>This is the about page</h1>
+    <p>This is the about page for our project called TraineeBee</p>
+
+@endsection
---
+++ TraineeBee/resources/views/Pages/candcinfo.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+@section('content')
+
+    <h1>This is the company and college information page</h1>
+    <p>This is the company and college information page for our project called TraineeBee</p>
+
+
+
+@endsection
