9949391253a9829d9d5197259d110466f42c5bdb
jorden <jorden2208@gmail.com>
2019-03-26 14:38:22 +0100

    added navbar **not working yet

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,12 +1,8 @@
-{{--these lines need to be in every view--}}
-{{--from here--}}
 @extends('Layouts.layout')
 @section('content')
-{{--all te content goes here--}}
 
     <p>hi</p>
 
 
 
-{{--ends the section this is also needed in every view / Blade page with content--}}
 @endsection
