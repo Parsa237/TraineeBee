4f7697e062aa907557333a9ecaf11cc8c1d84676
Niels M <niels.mooren@hotmail.nl>
2019-03-26 18:20:23 +0100

    update database

--- TraineeBee/config/database.php
+++ TraineeBee/config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql0.mydevil.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'm11668_TB'),
+            'username' => env('DB_USERNAME', 'm11668_TB'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
