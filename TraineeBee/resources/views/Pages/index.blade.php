7567e1923a7c28a89830ba2bceb12c9f64876de4
jorden <jorden2208@gmail.com>
2019-03-29 13:17:07 +0100

    Index page updated
    ONEPAGER!!!!

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,7 +1,14 @@
 <!-- extending the standard layout -->
 @extends('Layouts.layout')
 @section('content')
-    <h1>This is the index page</h1>
-    <p>This is the index page for our project called TraineeBee</p>
+    <div class="container-fluid">
+        <div id="firstPartNav">
+        <a href="/">Home</a>
+            <a href="/about">About</a>
+        </div>
+        <img id="logo" src="images/logo.png" alt="">
+    </div>
+        <h1>This is the index page</h1>
+        <p>This is the index page for our project called TraineeBee</p>
 
 @endsection
