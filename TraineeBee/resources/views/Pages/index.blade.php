893be368965630412b9e7a87d43d658204d948d3
jorden <jorden2208@gmail.com>
2019-03-29 15:08:30 +0100

    stingbutton good position

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -4,7 +4,6 @@
     <h1>This is the index page</h1>
     <p>This is the index page for our project called TraineeBee</p>
 
-    <div class="container" id="stingButton">
         <img id="stingButtonImg" src="images/stingButton.png" alt="">
-    </div>
+
 @endsection
