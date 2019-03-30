127fa1f8e37efb6482fa84aeeda582d0acb81cdc
jorden <jorden2208@gmail.com>
2019-03-30 08:45:49 +0100

    working on the sting button

--- TraineeBee/resources/views/Layouts/layout.blade.php
+++ TraineeBee/resources/views/Layouts/layout.blade.php
@@ -1,6 +1,7 @@
 <html lang="{{config('app.locale')}}">
 <head>
     <title>{{config('app.name', 'TraineeBee')}}</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
@@ -49,6 +50,8 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
+<script>
 
+</script>
 </body>
 </html>
