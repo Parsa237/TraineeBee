c1815f68fdfe6714cdea2bd1392f388675bd5c1a
jorden <jorden2208@gmail.com>
2019-03-30 08:48:57 +0100

    Revert "working on the sting button"
    This reverts commit 127fa1f8e37efb6482fa84aeeda582d0acb81cdc.

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -3,25 +3,7 @@
 @section('content')
     <h1>This is the index page</h1>
     <p>This is the index page for our project called TraineeBee</p>
-    <script>
-        $(function () {
-            var $win = $(window);
 
-            $win.scroll(function () {
-                if ($win.scrollTop() == 0){
-                    alert("ja");
-                }else {
-                    $("#stingButtonImg").animate({
-                        position: 'fixed',
-                        left: '47vw',
-                        transform: 'rotate(80deg)',
-                        width: '6vw'
-                    }, 2000);
-                }
-            });
-        });
-    </script>
-    <img id="stingButtonImg" src="images/stingButton.png" alt="">
-
+        <img id="stingButtonImg" src="images/stingButton.png" alt="">
 
 @endsection
