b2b35185d5889273e3a403458970d0eb574b5cb9
jorden <jorden2208@gmail.com>
2019-03-30 08:58:15 +0100

    sh**t

--- TraineeBee/app/User.php
+++ TraineeBee/app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $attributes = [
+        'status' => 0,
+        'isAdmin' => false
+    ];
+
 }
