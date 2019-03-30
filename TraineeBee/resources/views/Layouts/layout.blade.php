6079077fad3380978d439ebe03db50db2f13a3cb
jorden <jorden2208@gmail.com>
2019-03-30 09:02:20 +0100

    Revert "Styling"
    This reverts commit b8c3d8b96193aecfe03268a7d371340c85c18516.

--- TraineeBee/resources/views/Layouts/layout.blade.php
+++ TraineeBee/resources/views/Layouts/layout.blade.php
@@ -70,23 +70,23 @@
     <body>
         <div class="background">
             @guest
-                <div class="container-fluid nav pr-0 pt-5">
-                    <div class="row navRow justify-content-center pl-4 pr-4">
-                        <div class="col-2">
-                            <a class="firstPartNav" href="/">Home</a>
-                        </div>
-                        <div class="col-2">
-                            <a class="firstPartNav" href="/about">About</a>
-                        </div>
-                        <div class="col-4"></div>
-                        <div class="col-2">
-                            <a class="secondPartNav" href="/info">Information</a>
-                        </div>
-                        <div class="col-2">
-                            <a class="secondPartNav" href="/login">Login/register</a>
-                        </div>
+            <div class="container-fluid nav pr-0 pt-5">
+                <div class="row navRow justify-content-center pl-4 pr-4">
+                    <div class="col-2">
+                        <a class="firstPartNav" href="/">Home</a>
+                    </div>
+                    <div class="col-2">
+                        <a class="firstPartNav" href="/about">About</a>
+                    </div>
+                    <div class="col-4"></div>
+                    <div class="col-2">
+                        <a class="secondPartNav" href="/info">Information</a>
+                    </div>
+                    <div class="col-2">
+                        <a class="secondPartNav" href="/login">Login/register</a>
                     </div>
                 </div>
+            </div>
             @else
                 <div class="container-fluid nav pr-0 pt-5">
                     <div class="row navRow justify-content-center pl-4 pr-4">
