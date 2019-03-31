8e4a63d1141f1fd3e3fb458d20806f7cd87621cc
NielsBattle <35956413+NielsBattle@users.noreply.github.com>
2019-03-31 23:58:59 +0200

    Typo

--- TraineeBee/resources/views/Posts/show.blade.php
+++ TraineeBee/resources/views/Posts/show.blade.php
@@ -4,7 +4,7 @@
     @guest
         <p>please log in</p>
     @else
-        <a href="/dashboard" clas="btn btn-default">Go Back</a>
+        <a href="/dashboard" class="btn btn-default">Go Back</a>
         <h1>{{$post->title}}</h1>
         <div>{{$post->body}}</div>
         <small>Posted on {{$post->created_at}}</small>
