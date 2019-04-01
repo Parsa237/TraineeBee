56f77f905d25405fdd0aa9b6f418c9f9fb0092e1
TRichardson911 <1075220>
2019-04-01 13:36:15 +0100

    Dashboard

--- TraineeBee/resources/views/Pages/dashboard.blade.php
+++ TraineeBee/resources/views/Pages/dashboard.blade.php
@@ -5,20 +5,22 @@
     @else
         <h1>Offers:</h1>
             @if(count($posts) > 0)
-                @foreach($posts as $post)
-                    <div class="card card-body bg-light p-3">
-                        
-                        <font color='#222426' size='2pt'>
-                            <h1><a href="/dashboard/{{$post->id}}"  class="HeaderFontDash">{{$post->title}}</a></h1>
-                <h1>
-                    <p class="DescHeaderDash">Job Title: {{$post->title}}</p>
-                            </h1>
-                        <p>{{$post->body}}</p>
-                <h1>
-                  <p class="DescHeaderDash">Job Description: {{$post->title}}</p>
-                        </h1>
-                            <small>Posted on {{$post->created_at}}</small>
-                    </div>
+                @foreach($posts as $post)           
+            <div class=" card-body bg-light p-3 grid-container">
+                        <div>hafiyhefiuqefh</div>
+                        <div>
+                                <font color='#222426' size='2pt'/>
+                                    <h1><a href="/dashboard/{{$post->id}}"  class="HeaderFontDash">{{$post->title}}</a></h1>
+                        <h1>
+                            <p class="DescHeaderDash">Job Title: {{$post->title}}</p>
+                                    </h1>
+                                <p>{{$post->body}}</p>
+                        <h1>
+                          <p class="DescHeaderDash">Job Description: {{$post->title}}</p>
+                                </h1>
+                                    <small>Posted on {{$post->created_at}}</small>
+                        </div>
+            </div>
                 @endforeach
             {{$posts->links()}}
         @else
