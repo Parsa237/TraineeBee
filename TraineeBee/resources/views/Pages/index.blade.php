b3c8dd915a0c9a500ce47f8a6d1fc89fd304f665
Parsa <32632126+Parsa237@users.noreply.github.com>
2019-04-01 14:12:18 +0200

    changed the text

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,7 +1,7 @@
 <!-- extending the standard layout -->
 @extends('Layouts.layout')
 @section('content')
-    <h1>This is the index page</h1>
+    <h1>Welcome to TraineeBee!</h1>
     <p>This is the index page for our project called TraineeBee</p>
 
         <img id="stingButtonImg" src="images/stingButton2.png" alt="">
