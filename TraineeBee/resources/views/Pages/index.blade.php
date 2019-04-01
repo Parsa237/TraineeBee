dcd1c3f49b59bc145ee70d5f7d6ff8144b57e821
jorden <jorden2208@gmail.com>
2019-04-01 15:02:24 +0200

    removed line

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -7,21 +7,21 @@
 
 
 
-    <p>Welcome to TraineeBee!<br>
+    <p>
         The Place on the internet to find an internship at the best companies in and around the U.K.!<br>
         For a range of levels, studies and skills.
         Next to that it’s always good to go to places who have been rated on their performance!<br>
         And making sure to have fun and learn at the internship.
         <br>
-        If you are a company and you want to know why you should join our hive? Click here!
+        If you are a company and you want to know why you should join our hive? <a href="#info">Click here!</a>
         <br>
-        Want to know more about our Project!
+        Want to know more about our project!
     </p>
     <h1 id="about">
         About
     </h1>
     <h1 id="info">
-      info
+      Info
     </h1>
 
 
