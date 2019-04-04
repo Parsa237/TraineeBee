4449173439d1f96b57961f3604ed2e9845c91c0e
JoshuaDorrington <10752989@cityplym.ac.uk>
2019-04-04 14:58:04 +0200

    4rted

--- TraineeBee/resources/views/auth/register.blade.php
+++ TraineeBee/resources/views/auth/register.blade.php
@@ -63,15 +63,84 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="background-color:#53760D;color:#D0F18F" >
+                                <button type="submit" id="Submit_Button" class="btn btn-primary" style="background-color:#53760D; color:#D0F18F" disabled>
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
+                    </form>
+
+                    <!-- Captcha Test -->
+                    <form onsubmit="return checkform(this);">
+                        <br>
+                        <div class="capbox">
+
+                            <div id="CaptchaDiv"></div>
+
+                            <div class="capbox-inner">
+                            Type the above number:<br>
+
+                            <input type="hidden" id="txtCaptcha">
+                            <input type="text" name="CaptchaInput" id="CaptchaInput" size="15"><br>
+
+                            </div>
+                        </div>
+                        <br><br>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+
+
+<script type="text/javascript">
+
+function checkform(theform){
+var why = "";
+
+if(theform.CaptchaInput.value == ""){
+    why += "- Please Enter CAPTCHA Code.\n";
+}
+if(theform.CaptchaInput.value != ""){
+    if(ValidCaptcha(theform.CaptchaInput.value) == false){
+    why += "- The CAPTCHA Code Does Not Match.\n";
+    }
+}
+if(why != ""){
+    alert(why);
+    return false;
+}
+}
+
+var a = Math.ceil(Math.random() * 9)+ '';
+var b = Math.ceil(Math.random() * 9)+ '';
+var c = Math.ceil(Math.random() * 9)+ '';
+var d = Math.ceil(Math.random() * 9)+ '';
+var e = Math.ceil(Math.random() * 9)+ '';
+
+var code = a + b + c + d + e;
+document.getElementById("txtCaptcha").value = code;
+document.getElementById("CaptchaDiv").innerHTML = code;
+
+// Validate input against the generated number
+function ValidCaptcha(){
+var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+var str2 = removeSpaces(document.getElementById('CaptchaInput').value);
+if (str1 == str2){
+    document.getElementById("Submit_Button").disabled = false;
+    /*return true;*/
+}else{
+return false;
+}
+}
+
+// Remove the spaces from the entered and generated code
+function removeSpaces(string){
+return string.split(' ').join('');
+}
+</script>
+
 @endsection
