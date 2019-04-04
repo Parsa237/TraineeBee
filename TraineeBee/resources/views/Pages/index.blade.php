e4f521dd74f8c34023b2736ce66a4c5001dc60cc
jorden <jorden2208@gmail.com>
2019-04-04 13:15:11 +0200

    Revert "conflict"
    This reverts commit ba45a28d3931ccb1dbbce0d3887fefcdf9cfbe6b, reversing
    changes made to a0391fc47914872fdb53e7edccbde087c67f6497.

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,18 +1,22 @@
 <!-- extending the standard layout -->
 @extends('Layouts.layout')
 @section('content')
-    <head><link href="http://127.0.0.1:8000/css/aboutStyle.css" rel="stylesheet" type="text/css"></head>
-    <img id="stingButtonImg" src="images/stingButton2.png" alt="">
+    <head>
+        <link href="http://127.0.0.1:8000/css/aboutStyle.css" rel="stylesheet" type="text/css">
+    </head>
+
     <h1 id="welcome">Welcome to TraineeBee!</h1>
 
 
 
     <p>
-      A friendly environment for you to easily find internships from all around the UK!
-Internships give you the opportunity to experience the industry you are entering, have hands on experience and will give you
- the confidence when it comes to the workplace.
-Not only do
- we cater to interns but also to the amazing companies who can gain a lot from interns! Want to learn more about how to apply for internships and “join the hive”? Click here! <br>
+        A friendly environment for you to easily find internships from all around the UK!
+        Internships give you the opportunity to experience the industry you are entering, have hands on experience and
+        will give you
+        the confidence when it comes to the workplace.
+        Not only do
+        we cater to interns but also to the amazing companies who can gain a lot from interns! Want to learn more about
+        how to apply for internships and “join the hive”? Click here! <br>
 
         If you are a company and you want to know why you should join our hive? <a href="#info">Click here!</a>
         <br>
@@ -23,47 +27,55 @@ Not only do
     <div class="background">
 
 
-    <div class="flex-center position-ref full-height">
-          </div>  <div class="col-md-4 Pictures float-right" >
+        <div class="flex-center position-ref full-height">
+        </div>
+        <div class="col-md-4 Pictures float-right">
             <!-- first picture-->
-            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+            <img src="{{asset('images/placeholder.png')}}"
+                 alt="This a picture of the Group!  The dutch and The English peeps :D">
 
             <div class="col-md-6 Blank">wejhuirh</div>
-            <div class="col-md-6 Hexagon ">
 
-            </div>
 
             <!-- second picture-->
-            <div class="col-md-6 Hexagon">
 
-            </div>
             <div class="col-md-6 Blank ">ehwfeuife</div>
             <div class="col-md-6 Blank"></div>
             <!-- third picture-->
-            <div class="col-md-6 Hexagon float-right">
-            </div>
+
             <div class="col-md-6 Blank">weuryu9wi</div>
 
         </div>
-      <div class="col-md-8 p-0 No_Margin">
-      <h1>About the project</h1>
-    <p>Trainee Bee
+        <div class="col-md-8 p-0 No_Margin">
+            <h1>About the project</h1>
+            <p>Trainee Bee
             <div class="smaller p-0">Join the Hive TM</div>
             Who are we?
+
+            We are a collection of 13 students from the Netherlands and Britain working on a project together to promote
+            internships into the English education system.
+            Why are we doing this?
+            Our main goal is to promote internships, programs that while in education, students get a hands-on
+            experience in the work place that allows them to work with a company for a long duration of time. They also
+            allow companies to see upcoming prodigies, get to work with students who are motivated and possibly find a
+            future employee.
+
 We are a collection of 13 students from the Netherlands and Britain working on a project together to promote internships into the English education system.
 Why are we doing this?
-Our main goal is to promote internships, programs that while in education, students get a hands-on experience in the work place that allows them to work with a company for a long duration of time. They also allow companies to see upcoming prodigies, get to work with students who are motivated and possibly find a future employee.
+Our main goal is to promote internships, programs that while in education, students get a hands-on experience in the work place that allows them to work with a company for a long duration of time. They also allow companies to see upcoming prodigies, get to work with students who are motivated and possibly find a future employee. Following a trip to the Netherlands the British students came to realise that completing an internship in the U.K could boost their job prospects dramatically. Interns have been proven to be very valuable to companies and bringing it to the U.K would increase the quality of knowledge that students have.
+
 
             </p>
         </div>
 
 
         <div class="col-md-12 Picture">
-            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+            <img src="{{asset('images/placeholder.png')}}"
+                 alt="This a picture of the Group!  The dutch and The English peeps :D">
         </div>
         <div class="col-md-4 Pictures"></div>
         <div class="col-md-8 p-0">
-        <h1>About Us</h1>
+            <h1>About Us</h1>
             <p>This is the about page for our project called TraineeBee</p>
         </div>
     </div>
@@ -74,4 +86,11 @@ Our main goal is to promote internships, programs that while in education, stude
     </h1>
 
 
+    @yield('footer')
+
+    <script>
+        $(window).ready(function () {
+            $isReady++;
+        });
+    </script>
 @endsection
