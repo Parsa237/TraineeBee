f8eed826268a6ae6c27391c5a66c5ef2b4565e67
AntiEevee <1038358@idcollege.nl>
2019-04-04 09:42:28 +0200

    HomepageV1.3

--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,7 +1,7 @@
 <!-- extending the standard layout -->
 @extends('Layouts.layout')
 @section('content')
-
+    <head><link href="http://127.0.0.1:8000/css/aboutStyle.css" rel="stylesheet" type="text/css"></head>
     <img id="stingButtonImg" src="images/stingButton2.png" alt="">
     <h1 id="welcome">Welcome to TraineeBee!</h1>
 
@@ -18,28 +18,57 @@
         Want to know more about our project!
     </p>
 
-    <h1 id="about">
-        About
-    </h1>
-    <h2></h2>
-    <p>Trainee Bee
-    <div class="smaller">Join the Hive TM</div>
-        is an Exange Program between the Netherlands and England for four weeks in total.<br>
-        They have been working on this project and had allot of fun with it.<br>
-        <br>
-        In the exchange the Dutch students showed the places where they went to for theire internship,<br>
-        To showed them what it is like and how this will function in theire education<br>
-        and What type of profits it has for the student and companies.
-        (To know more about what the profits are about Internships in your <a href="#info" class="Links">company</a>.
-        The students interviewed the companies as well and got exited about the amount they were missing. <br>
-        <br>
-        The students will also learn how they work in different countries, what their work progress is like and in big
-        international groups which is a great move for all the students in the future.<br> The two groups showed them
-        their city. From the begin it was very clear what we wanted. We wanted a site which make it possible to have
-        internships in the UK as well
-        and to Help Students College and Companies their way through this new way.
-    </p>
+    <div class="background">
 
+
+        <div class="flex-center position-ref full-height">
+        </div>  <div class="col-md-4 Pictures float-right" >
+            <!-- first picture-->
+            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+
+            <div class="col-md-6 Blank">wejhuirh</div>
+            <div class="col-md-6 Hexagon ">
+
+            </div>
+
+            <!-- second picture-->
+            <div class="col-md-6 Hexagon">
+
+            </div>
+            <div class="col-md-6 Blank ">ehwfeuife</div>
+            <div class="col-md-6 Blank"></div>
+            <!-- third picture-->
+            <div class="col-md-6 Hexagon float-right">
+            </div>
+            <div class="col-md-6 Blank">weuryu9wi</div>
+
+        </div>
+        <div class="col-md-8 No_Margin">
+            <h1>About the Project</h1>
+            <p>Trainee Bee <div class="smaller">Join the Hive TM</div> is an Exange Program between the Netherlands and England for four weeks in total.<br>
+            they have been working on this project. And had allot of fun with it.<br>
+            <br>
+            In the exchange the Dutch people showed the places where they went to for theire internship.<br>
+            To showed them what it is like and how this will function in theire education.<br>
+            And What type of profits it has for the student and companies.
+            (To know more about what the profits are about Internships in your <a href="#info" class="Links">company</a>.
+            The students interviewed the companies as well and got exited about the amount they were missing. <br>
+            <br>
+            The students will also learn how they work in different countries, what their work progress is like. And in big international groups which is a great move for all the students in the future.<br> The two groups showed them their city. From the begin it was very clear what we wanted. We wanted a site which make it possible to have internships in the UK as well.
+            And to Help Students College and Companies their way through this new way.
+            </p>
+        </div>
+
+        <div class="col-md-12 Picture">
+            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+        </div>
+        <div class="col-md-4 Pictures"></div>
+        <div class="col-md-8">
+            <h1>About Us</h1>
+            <p>This is the about page for our project called TraineeBee</p>
+        </div>
+    </div>
+    </body>
 
     <h1 id="info">
         Information
