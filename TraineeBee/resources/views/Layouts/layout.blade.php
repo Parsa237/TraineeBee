a0759a320856e0179f7ca671fa330050f8d304fc
jorden <jorden2208@gmail.com>
2019-04-04 11:48:49 +0200

    button fully working

--- TraineeBee/resources/views/Layouts/layout.blade.php
+++ TraineeBee/resources/views/Layouts/layout.blade.php
@@ -10,8 +10,21 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
 </head>
 <body>
+<script>
+    $isReady = 0;
+</script>
 <a class="logoNav" href="/"><img id="logo" src="{{asset('images/banner_logo.png')}}" alt="logo"
                                  href="/home"></a>
 <div class="background">
@@ -87,86 +100,78 @@
 
     </div>
 
-    <footer class="page-footer font-small blue pt-4 footer">
+        <footer class="page-footer font-small blue pt-4 footer">
 
-        <!-- Footer Links -->
-        <div class="container-fluid text-center text-md-left">
+            <!-- Footer Links -->
+            <div class="container-fluid text-center text-md-left">
 
-            <!-- Grid row -->
-            <div class="row">
+                <!-- Grid row -->
+                <div class="row">
 
-                <!-- Grid column -->
-                <div class="col-md-6 mt-md-0 mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-6 mt-md-0 mt-3">
 
-                    <!-- Content -->
-                    <h5 class="text-uppercase">TraineeBee</h5>
-                    <p>Feel free to contact us if you have any suggestions about our page. </p>
+                        <!-- Content -->
+                        <h5 class="text-uppercase">TraineeBee</h5>
+                        <p>Feel free to contact us if you have any suggestions about our page. </p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="clearfix w-100 d-md-none pb-3">
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                        <!-- Links -->
+                        <h5 class="text-uppercase">headings</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="./#about">About us<a>
+                            </li>
+                            <li>
+                                <a href="./#info">Information</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                        <!-- Links -->
+                        <h5 class="text-uppercase">Contact</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Login</a>
+                            </li>
+                            <li>
+                                <a href="#!">Contact us!</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <!-- Grid column -->
 
                 </div>
-                <!-- Grid column -->
-
-                <hr class="clearfix w-100 d-md-none pb-3">
-
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
-
-                    <!-- Links -->
-                    <h5 class="text-uppercase">headings</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="./#about">About us<a>
-                        </li>
-                        <li>
-                            <a href="./#info">Information</a>
-                        </li>
-
-                    </ul>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
-
-                    <!-- Links -->
-                    <h5 class="text-uppercase">Contact</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Login</a>
-                        </li>
-                        <li>
-                            <a href="#!">Contact us!</a>
-                        </li>
-
-                    </ul>
-
-                </div>
-                <!-- Grid column -->
+                <!-- Grid row -->
 
             </div>
-            <!-- Grid row -->
 
-        </div>
+            <!-- Footer Links -->
 
-        <!-- Footer Links -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3 ">© 2018 Copyright:
-            Traineebee
-        </div>
-    </footer>
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3 ">© 2019 Copyright:
+                Traineebee
+            </div>
+        </footer>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
+
     <script>
         $(window).scroll(function () {
             if ($(this).scrollTop() > 10) { // this refers to window
@@ -191,6 +196,10 @@
                 });
                 $i++;
             }
+            if($isReady > 0){
+                $(document.body).append('<img id=\'stingButtonImg\' src=\'images/stingButton2.png\' alt=\'\'>');
+            }
+
         });
 
 
@@ -201,6 +210,8 @@
                     } );
             }
         });
+        // <img id="stingButtonImg" src="images/stingButton2.png" alt="">
+
     </script>
 </div>
 </body>
--- TraineeBee/resources/views/Pages/index.blade.php
+++ TraineeBee/resources/views/Pages/index.blade.php
@@ -1,18 +1,22 @@
 <!-- extending the standard layout -->
 @extends('Layouts.layout')
 @section('content')
-    <head><link href="http://127.0.0.1:8000/css/aboutStyle.css" rel="stylesheet" type="text/css"></head>
+    <head>
+        <link href="http://127.0.0.1:8000/css/aboutStyle.css" rel="stylesheet" type="text/css">
+    </head>
 
     <h1 id="welcome">Welcome to TraineeBee!</h1>
 
 
 
     <p>
-      A friendly environment for you to easily find internships from all around the UK!
-Internships give you the opportunity to experience the industry you are entering, have hands on experience and will give you
- the confidence when it comes to the workplace.
-Not only do
- we cater to interns but also to the amazing companies who can gain a lot from interns! Want to learn more about how to apply for internships and “join the hive”? Click here! <br>
+        A friendly environment for you to easily find internships from all around the UK!
+        Internships give you the opportunity to experience the industry you are entering, have hands on experience and
+        will give you
+        the confidence when it comes to the workplace.
+        Not only do
+        we cater to interns but also to the amazing companies who can gain a lot from interns! Want to learn more about
+        how to apply for internships and “join the hive”? Click here! <br>
 
         If you are a company and you want to know why you should join our hive? <a href="#info">Click here!</a>
         <br>
@@ -23,10 +27,12 @@ Not only do
     <div class="background">
 
 
-    <div class="flex-center position-ref full-height">
-          </div>  <div class="col-md-4 Pictures float-right" >
+        <div class="flex-center position-ref full-height">
+        </div>
+        <div class="col-md-4 Pictures float-right">
             <!-- first picture-->
-            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+            <img src="{{asset('images/placeholder.png')}}"
+                 alt="This a picture of the Group!  The dutch and The English peeps :D">
 
             <div class="col-md-6 Blank">wejhuirh</div>
 
@@ -40,25 +46,30 @@ Not only do
             <div class="col-md-6 Blank">weuryu9wi</div>
 
         </div>
-      <div class="col-md-8 p-0 No_Margin">
-      <h1>About the project</h1>
-    <p>Trainee Bee
+        <div class="col-md-8 p-0 No_Margin">
+            <h1>About the project</h1>
+            <p>Trainee Bee
             <div class="smaller p-0">Join the Hive TM</div>
             Who are we?
-We are a collection of 13 students from the Netherlands and Britain working on a project together to promote internships into the English education system.
-Why are we doing this?
-Our main goal is to promote internships, programs that while in education, students get a hands-on experience in the work place that allows them to work with a company for a long duration of time. They also allow companies to see upcoming prodigies, get to work with students who are motivated and possibly find a future employee.
+            We are a collection of 13 students from the Netherlands and Britain working on a project together to promote
+            internships into the English education system.
+            Why are we doing this?
+            Our main goal is to promote internships, programs that while in education, students get a hands-on
+            experience in the work place that allows them to work with a company for a long duration of time. They also
+            allow companies to see upcoming prodigies, get to work with students who are motivated and possibly find a
+            future employee.
 
             </p>
         </div>
 
 
         <div class="col-md-12 Picture">
-            <img src="{{asset('images/placeholder.png')}}" alt="This a picture of the Group!  The dutch and The English peeps :D">
+            <img src="{{asset('images/placeholder.png')}}"
+                 alt="This a picture of the Group!  The dutch and The English peeps :D">
         </div>
         <div class="col-md-4 Pictures"></div>
         <div class="col-md-8 p-0">
-        <h1>About Us</h1>
+            <h1>About Us</h1>
             <p>This is the about page for our project called TraineeBee</p>
         </div>
     </div>
@@ -68,5 +79,12 @@ Our main goal is to promote internships, programs that while in education, stude
         Information
     </h1>
 
-    <img id="stingButtonImg" src="images/stingButton2.png" alt="">
+
+    @yield('footer')
+
+    <script>
+        $(window).ready(function () {
+            $isReady++;
+        });
+    </script>
 @endsection
